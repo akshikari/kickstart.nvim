a7aaa8899733cc197e74d88c2c714ee755e3f0b7
Abhi Kumar <abhinavbkumar@protonmail.com>
2024-10-15 09:10:30 -0500

    Added auto command from gopls docs to auto-import dependencies

--- init.lua
+++ init.lua
@@ -142,6 +142,32 @@ vim.api.nvim_create_autocmd('BufReadPost', {
   end,
 })
 
+-- [[ Specific Autocommands ]]
+-- Things I want to happen not from kickstart
+
+-- Auto-import dependencies as I type and organize imports (when possible)
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { 'source.organizeImports' } }
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format { async = false }
+  end,
+})
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -606,6 +632,7 @@ require('lazy').setup({
         'prettier', -- TypeScript, JavaScript, Markdown, HTML, CSS Formatter
         'prettierd', -- More performant formatter
         'markdownlint', -- Markdown linter
+        'golangci-lint', -- Golang linter
         'hadolint', -- Dockerfile linter
         'jsonlint', -- JSON linter
         'eslint', -- JavaScript linter
@@ -965,7 +992,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>a', function()
         harpoon:list():add()
       end, { desc = '[A]dd file to harpoon pick list' })
-      vim.keymap.set('n', '<C-Space>', function()
+      vim.keymap.set('n', '<C-m>', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end, { desc = 'Toggle harpoon list' })
 
